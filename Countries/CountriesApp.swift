5351e287427448ce0e6ce7ee5a0631138c374dc5
karl <northrupkarl@gmail.com>
2025-10-05 22:37:21 -0500

    create base SwiftUI project and folder structure

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by karl on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CountryListView()
+        }
+    }
+}
---
+++ Countries/Views/CountryListView.swift
@@ -0,0 +1,24 @@
+//
+//  CountryListView.swift
+//  Countries
+//
+//  Created by karl on 10/4/25.
+//
+
+import SwiftUI
+
+struct CountryListView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    CountryListView()
+}
